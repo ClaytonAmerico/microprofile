dbcf8da7c8e6509f363cfc7a6f14689a33f847ba
Clayton <clayton.americo@gmail.com>
2020-10-25 19:55:05 -0300

    Novos ajustes testes

--- src/test/java/com/github/clayton/microprofile/HelloResourceTest.java
+++ src/test/java/com/github/clayton/microprofile/HelloResourceTest.java
@@ -1,22 +1,5 @@
 package com.github.clayton.microprofile;
 
-import io.quarkus.runtime.annotations.QuarkusMain;
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
-@QuarkusMain
 public class HelloResourceTest {
-
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
 
 }
--- src/test/java/com/github/clayton/microprofile/NativeHelloResourceIT.java
+++ src/test/java/com/github/clayton/microprofile/NativeHelloResourceIT.java
@@ -1,8 +1,5 @@
 package com.github.clayton.microprofile;
 
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
 public class NativeHelloResourceIT extends HelloResourceTest {
 
     // Execute the same tests but in native mode.

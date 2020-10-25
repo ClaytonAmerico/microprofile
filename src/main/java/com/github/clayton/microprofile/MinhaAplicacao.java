f71ae3e5c429092f31fd5faaf9c0eebc178565c4
Clayton <clayton.americo@gmail.com>
2020-10-25 19:43:07 -0300

    Commit Incial

---
+++ src/main/java/com/github/clayton/microprofile/MinhaAplicacao.java
@@ -0,0 +1,10 @@
+package com.github.clayton.microprofile;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/")
+public class MinhaAplicacao extends Application{
+
+}
